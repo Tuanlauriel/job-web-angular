c6ba6070d8dafad99a4d9660686ff46702796682
Nguyen Minh Hieu <ngmhieu1709@gmail.com>
2023-10-18 23:07:12 +0700

    testcommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { JobDetailComponent } from './pages/job-detail/job-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'tuanancut', component: JobDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
